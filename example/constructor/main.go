de451b09a532a290f5fb5dc7ba1ff4d18c6b8b4b
Robert MacWha <trebor.ahwcam@gmail.com>
2024-09-11 22:22:32 -0400

    feat: examples

---
+++ example/constructor/main.go
@@ -0,0 +1,59 @@
+package constructor
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Skylock-ai/safe/pkg/safe"
+)
+
+type ConnType uint
+type Client struct {
+	url      string
+	connType ConnType
+}
+
+const (
+	HTTP ConnType = iota
+	HTTPS
+)
+
+func SafeNewClient(url string) (res safe.Result[Client]) {
+	safe.Handle(&res)
+
+	if url == "" {
+		return safe.Err[Client](fmt.Errorf("missing url"))
+	}
+
+	if !strings.Contains(url, ":") {
+		return safe.Err[Client](fmt.Errorf("missing protocol"))
+	}
+
+	switch strings.Split(url, ":")[0] {
+	case "http":
+		return safe.Ok(Client{url: url, connType: HTTP})
+	case "https":
+		return safe.Ok(Client{url: url, connType: HTTPS})
+	default:
+		return safe.Err[Client](fmt.Errorf("unsupported protocol"))
+	}
+}
+
+func NewClient(url string) (*Client, error) {
+	if url == "" {
+		return nil, fmt.Errorf("missing url")
+	}
+
+	if !strings.Contains(url, ":") {
+		return nil, fmt.Errorf("missing protocol")
+	}
+
+	switch strings.Split(url, ":")[0] {
+	case "http":
+		return &Client{url: url, connType: HTTP}, nil
+	case "https":
+		return &Client{url: url, connType: HTTPS}, nil
+	default:
+		return nil, fmt.Errorf("unsupported protocol")
+	}
+}
---
+++ example/io/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	"github.com/Skylock-ai/safe/pkg/safe"
+)
+
+func SafeIoRead(path string) (res safe.Result[string]) {
+	safe.Handle(&res)
+
+	file := safe.Res(os.Open(path)).Unwrap()
+	defer file.Close()
+
+	contents := safe.Res(io.ReadAll(file)).Unwrap()
+	return safe.Ok(string(contents))
+}
+
+func IoRead(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	contents, err := io.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+	return string(contents), nil
+}
